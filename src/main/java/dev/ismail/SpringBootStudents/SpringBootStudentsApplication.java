d0c35f74988b63ba4dfdd16ec2f77c6edcb02f76
Ismail <ismail98ozkan@gmail.com>
2024-03-07 07:18:39 +0100

    Punto Inicial

---
+++ src/main/java/dev/ismail/SpringBootStudents/SpringBootStudentsApplication.java
@@ -0,0 +1,13 @@
+package dev.ismail.SpringBootStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ismail/SpringBootStudents/SpringBootStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ismail.SpringBootStudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
