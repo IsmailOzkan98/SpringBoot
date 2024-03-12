923022b70ed068c75b099b8a2cc5fabaf26af4c9
Ismail <ismail98ozkan@gmail.com>
2024-03-12 12:03:09 +0100

    added new feature for weather api

--- src/main/java/dev/ismail/SpringBootStudents/controller/WeatherController.java
+++ src/main/java/dev/ismail/SpringBootStudents/controller/WeatherController.java
@@ -3,23 +3,35 @@ package dev.ismail.SpringBootStudents.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RestController
 public class WeatherController {
 
-    private static final String OpenWeatherApiurl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ad21d76160cbf5e6962cc15717035bfe";
+
+    static final String API_KEY = "ad21d76160cbf5e6962cc15717035bfe";
+
+
+    //private static final String OpenWeatherApiurl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ad21d76160cbf5e6962cc15717035bfe";
+    //private static final String OpenWeatherApiurl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid="+API_KEY;
 
     @GetMapping("/api/v1/weather")
-    public ResponseEntity<String> getWeatherInfo() {
+    public ResponseEntity<Map> getWeatherInfo(
+            @RequestParam String city,
+            @RequestParam String country) {
+
+        String OpenWeatherApiURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +","+ country +"&appid="+API_KEY;
+
         RestTemplate restTemplate = new RestTemplate();
-        String weatherInfo = restTemplate.getForObject(OpenWeatherApiurl, String.class);
+        Map weatherInfo = restTemplate.getForObject(OpenWeatherApiURL, Map.class);
 
         return new ResponseEntity<>(weatherInfo, HttpStatus.OK);
     }
 }
-
 
 
 
